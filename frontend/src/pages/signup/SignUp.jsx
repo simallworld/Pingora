b105f3f46340262c3259781bb1d10cf4183126f8
Shivam Swaroop <shivamswaroop89@gmail.com>
2025-08-19 15:50:36 +0530

    Bug fixed relatd to red color

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -32,7 +32,7 @@ const SignUp = () => {
 
 				<form onSubmit={handleSubmit}>
 					<div>
-						<label className='label p-2 text-red-300'>
+						<label className='label p-2'>
 							<span className='text-base label-text'>Full Name</span>
 						</label>
 						<input
